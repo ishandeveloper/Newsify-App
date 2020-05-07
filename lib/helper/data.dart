cfe4685d2030bd1404cfd0cfd6bb5cd35e8a2b98
Ishan Sharma <ishandeveloper@outlook.com>
2020-05-07 14:11:41 +0530

    updatedData

--- lib/helper/data.dart
+++ lib/helper/data.dart
@@ -1,34 +1,34 @@
 import 'package:news_app/models/categoryModel.dart';
 
 List<CategoryModel> getCategories() {
-  List<CategoryModel> category = new List<CategoryModel>();
+  List<CategoryModel> myCategories = List<CategoryModel>();
+  CategoryModel categoryModel;
 
-  CategoryModel categoryModel = new CategoryModel();
+  //1
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Business";
+  categoryModel.imageURL = "assets/hostedImages/covid19.jpg";
+  myCategories.add(categoryModel);
 
-  // 1
-  categoryModel.categoryName = 'COVID-19';
-  categoryModel.imageURL =
-      'https://github.com/ishandeveloper/Newsify-App/blob/master/assets/hostedImages/covid19.jpg?raw=false';
-  category.add(categoryModel);
+  //2
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Entertainment";
+  categoryModel.imageURL = "assets/hostedImages/entertainment.jpg";
+  myCategories.add(categoryModel);
 
-  // 2
-  categoryModel.categoryName = 'Business';
-  categoryModel.imageURL =
-      'https://github.com/ishandeveloper/Newsify-App/blob/master/assets/hostedImages/business.jpg?raw=false';
-  category.add(categoryModel);
+  //3
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Business";
+  categoryModel.imageURL = "assets/hostedImages/business.jpg";
+  myCategories.add(categoryModel);
 
-  // 3
-  categoryModel.categoryName = 'Entertainment';
-  categoryModel.imageURL =
-      'https://github.com/ishandeveloper/Newsify-App/blob/master/assets/hostedImages/entertainment.jpeg?raw=false';
-  category.add(categoryModel);
+  //4
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "General";
+  categoryModel.imageURL = "assets/hostedImages/general.jpg";
+  myCategories.add(categoryModel);
 
-  // 4
-  categoryModel.categoryName = 'General';
-  categoryModel.imageURL =
-      'https://github.com/ishandeveloper/Newsify-App/blob/master/assets/hostedImages/general.jpg?raw=false';
-  category.add(categoryModel);
 
-  return category;
+  return myCategories;
 
 }
